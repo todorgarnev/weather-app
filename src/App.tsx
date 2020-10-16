1ceeafd0ee1381255c19dbadf92c9f57b4785dd8
Todor Garnev <tgarnev@abv.bg>
2020-10-16 20:42:41 +0300

    Added error handling message.

--- src/App.tsx
+++ src/App.tsx
@@ -8,43 +8,51 @@ import * as requestsUtil from './shared/utils/requestsUtil';
 const App = () => {
   const [cityName, setCityName] = useState<string>('');
   const [cityWeather, setCityWeather] = useState<Weather>();
+  const [error, setError] = useState<string>('');
 
   const getCityWeather = async () => {
     const data: any = await requestsUtil.getTodayCityWeather(cityName);
     setCityName('');
-    setCityWeather({
-      name: data.name,
-      weather: {
-        description: data.weather[0].description,
-        icon: data.weather[0].icon,
-        main: data.weather[0].main
-      },
-      main: {
-        feels_like: data.main.feels_like,
-        humidity: data.main.humidity,
-        pressure: data.main.pressure,
-        temp: data.main.temp,
-        temp_max: data.main.temp_max,
-        temp_min: data.main.temp_min
-      },
-      clouds: data.clouds.all,
-      wind: data.wind.speed,
-      sunrise: data.sys.sunrise,
-      sunset: data.sys.sunset
-    });
+
+    if (data.error) {
+      setError(data.message)
+    } else {
+      setCityWeather({
+        name: data.name,
+        weather: {
+          description: data.weather[0].description,
+          icon: data.weather[0].icon,
+          main: data.weather[0].main
+        },
+        main: {
+          feels_like: data.main.feels_like,
+          humidity: data.main.humidity,
+          pressure: data.main.pressure,
+          temp: data.main.temp,
+          temp_max: data.main.temp_max,
+          temp_min: data.main.temp_min
+        },
+        clouds: data.clouds.all,
+        wind: data.wind.speed,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset
+      });
+      setError('');
+    }
+
   }
 
   return (
-    <div className="app">
-      <div className="header">
-        <input type="text" className="search-bar" value={cityName} onChange={(e) => setCityName(e.target.value)} />
-        <span className="name-placeholder"></span>
+    <div className='app'>
+      <div className='header'>
+        <input type='text' className='search-bar' value={cityName} onChange={(e) => setCityName(e.target.value)} />
+        <span className='name-placeholder'></span>
 
-        <button className="search-button" onClick={getCityWeather} disabled={cityName === ''}>Search</button>
+        <button className='search-button' onClick={getCityWeather} disabled={cityName === ''}>Search</button>
       </div>
 
-      <div className="main">
-        {cityWeather && <Day weatherData={cityWeather} />}
+      <div className='main'>
+        {cityWeather ? <Day weatherData={cityWeather} /> : <div className="error">{error}</div>}
       </div>
     </div>
   );
--- src/shared/utils/requestsUtil.ts
+++ src/shared/utils/requestsUtil.ts
@@ -2,8 +2,12 @@ import axios, { AxiosResponse } from 'axios';
 import * as apiConstants from '../constants/apiConstants';
 
 const getTodayCityWeather = async (cityName: string) => {
-  const response: AxiosResponse = await axios.get(`${apiConstants.cityWeatherUrl}${cityName}${apiConstants.appId}`);
-  return response.data;
+  try {
+    const response: AxiosResponse = await axios.get(`${apiConstants.cityWeatherUrl}${cityName}${apiConstants.appId}`);
+    return response.data;
+  } catch (e) {
+    return { error: true, message: 'No city found' }
+  }
 }
 
 export { getTodayCityWeather };
